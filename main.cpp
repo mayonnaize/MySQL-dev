84b8cf8aa56e03d3655a4534e2f2e23f02249b47
mayonnaize <76809047+mayonnaize@users.noreply.github.com>
2023-12-29 07:10:20 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+    // 結果変数
+    // std::string result;
+
+    // 処理 =========================
+    int N;
+    std::cin >> N;
+    // 処理 =========================
+
+    // 結果表示
+    std::cout << N * 10 << std::endl;
+    return 0;
+}
