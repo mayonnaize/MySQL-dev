cec8ab2297e04505d83da51e2ab41f8a37e0b94b
mayonnaize <chart.154.vision@gmail.com>
2024-01-03 02:57:49 +0900

    cmake.hを整備

--- bin/connector/cmake.sh
+++ bin/connector/cmake.sh
@@ -3,17 +3,14 @@
 ls --ignore=*.sh | xargs rm -r
 clear
 
-cmake ../../mysql-connector-cpp \
+cmake /app/mysql-connector-cpp \
+    -DCMAKE_BUILD_TYPE=Debug \
     -DBOOST_ROOT=~/boost_1_59_0 \
     -DUSE_SERVER_CXXFLAGS=1 \
     -DMYSQL_CFLAGS="" \
-    -DMYSQL_CXXFLAGS="-stdlib=libc++ -Wno-deprecated-declarations -v" \
-    -DCMAKE_CXX_FLAGS="-stdlib=libc++ -Wno-deprecated-declarations -v" \
+    -DCMAKE_CXX_FLAGS="-stdlib=libc++ -Wno-deprecated-declarations" \
     -DMYSQL_DIR=/usr/local/mysql \
     -DMYSQL_INCLUDE_DIR=/usr/local/mysql/include \
     -DMYSQL_LIB_DIR=/usr/local/mysql/lib
 
-make VERBOSE=1
-
-# make -j 8
-    # -DMYSQL_CXXFLAGS="-stdlib=libc++ -std=gnu++1y -Wno-deprecated-declarations -v" \
+make -j 8
---
+++ bin/server/cmake.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ls --ignore=*.sh | xargs rm -r
+clear
+
+cmake /home/root/mysql-server \
+    -DWITHOUT_SERVER=ON \
+    -DDOWNLOAD_BOOST=1 \
+    -DWITH_BOOST=/home/root/boost_1_59_0 \
+    -DWITH_UNIT_TESTS=OFF \
+    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++"
+
+make -j 8
