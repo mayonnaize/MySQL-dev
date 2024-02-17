0efc0961cb953a895e28759cba538177b0a54bcf
mayonnaize <chart.154.vision@gmail.com>
2024-02-17 13:38:18 +0900

    ビルド出来た

--- .devcontainer/conn-build.sh
+++ .devcontainer/conn-build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cmake mysql-connector-cpp \
+clear
+
+cmake /home/root/mysql-connector-cpp \
     -DBOOST_ROOT=/home/root/boost_1_59_0 \
     -DUSE_SERVER_CXXFLAGS=1 \
     -DCMAKE_CXX_FLAGS="-stdlib=libc++ -Wno-deprecated-declarations" \
     -DBOOST_ROOT=/home/root/boost_1_72_0 \
-    -DMYSQL_INCLUDE_DIR=/usr/include/mysql \
-    -DMYSQL_LIB_DIR=/usr/lib/x86_64-linux-gnu
+    -DMYSQL_INCLUDE_DIR=/usr/local/mysql/include/ \
+    -DMYSQL_LIB_DIR=/usr/local/mysql/lib/
 
 make -j 8 && \
     make install
--- .devcontainer/server-build.sh
+++ .devcontainer/server-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+clear
+
 cmake /home/root/mysql-server \
     -DWITHOUT_SERVER=ON \
-    -DDOWNLOAD_BOOST=1 \
-    -DWITH_BOOST=/home/root/boost_1_72_0 \
     -DWITH_UNIT_TESTS=OFF \
+    -DWITH_BOOST=/home/root/boost_1_72_0/ \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++"
 
 make -j 8 && \
